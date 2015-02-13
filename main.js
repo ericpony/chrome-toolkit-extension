9d40a969a4c8b018cd9102891967194210c2566b
ericpony <ericpony@csie.org>
2015-02-14 06:56:21 +0800

    add image links

--- main.js
+++ main.js
@@ -1,18 +1,23 @@
 if(/^https:\/\/www.google.[^\/]+\/search/.test(location.href)) {
+	$.noConflict();
 	// turn off SafeSearch 
 	if(location.search.indexOf('safe=off')<0) {
 		location.replace(location.href + '&safe=off');
 	}else {
 		// replace the href of thumbnail with a link to the original image
-		$(document).ready(function() {
-    		console.log( "ready!" );
-			$('._lyb>a').each(function(i,a) {
+		jQuery(document).ready(function() {
+			jQuery('._lyb>a').each(function(i,a) {
 				if(!/imgurl=([^\&]+)/.test(a.href)) return;
 				a.href = RegExp.$1;
 				console.log(a.href);
 				a.onmousedown = undefined;
 				a.target = '_blank';
 			});
+			jQuery('.rg_l').each(function() {
+				var imgurl = decodeURIComponent(this.href.match(/url=[^&]+/)[0].match(/=.+/)[0].substr(1));
+				//this.style.position = 'relative';
+				jQuery(this.parentNode).prepend('<div style="background-color:white;z-index:10000;position:absolute" onclick="event.cancelBubble=true"><a href="' + imgurl + '" target="_blank">Open image</a></div>')
+			});	
 		});
 	}
 }
