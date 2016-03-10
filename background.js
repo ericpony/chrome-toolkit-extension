436aef7d8eb75a3928b106105361945be4552ae2
ericpony <ericpony@csie.org>
2016-03-10 11:34:21 +0800

    do not analyze links

--- background.js
+++ background.js
@@ -4,21 +4,21 @@ chrome.contextMenus.create({
 	title : "透過 IQDB 搜尋這張圖片 (&1)",
 	type  : "normal",
 	contexts : ['link','image'],
-	onclick: function (info, tab) {
-		var img_link 	=info.srcUrl || info.linkUrl || info.pageUrl;
-		if(!img_link) return;
-		chrome.tabs.create({ url: 'http://iqdb.org/?url=' + encodeURIComponent(img_link)});
-	}
+  onclick: function (info, tab) {
+    var img_link = info.srcUrl || info.linkUrl || info.pageUrl;
+    if (!img_link) return;
+    chrome.tabs.create({url: 'http://iqdb.org/?url=' + encodeURIComponent(img_link)});
+  }
 });
 chrome.contextMenus.create({
 	id    : 'parse',
 	title : "重新分析頁面 (&2)",
 	type  : "normal",
-	contexts : ['all'],
-	onclick: function (info, tab) {
-		chrome.tabs.executeScript(null, {file: 'lib/jquery-1.11.2.min.js'}, function() { 
-			chrome.tabs.executeScript(null, {file: 'main.js'});
-		});
-	}
+	contexts : ['page'],
+  onclick: function (info, tab) {
+    chrome.tabs.executeScript(null, {file: 'lib/jquery-1.11.2.min.js'}, function () {
+      chrome.tabs.executeScript(null, {file: 'main.js'});
+    });
+  }
 });
 
