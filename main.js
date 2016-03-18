3ca3f984012be88361b698cbb3796ab8aba327c8
ericpony <ericpony@csie.org>
2016-03-18 21:45:33 +0800

    update

--- main.js
+++ main.js
@@ -16,9 +16,9 @@ if (/^https:\/\/www.google.[^\/]+\/search/.test(location.href)) {
       jQuery('.rg_l').each(function (i) {
         var id = 'img-link-' + i;
         if (document.getElementById(id)) return true;
-        var imgurl = decodeURIComponent(this.href.match(/url=[^&]+/)[0].match(/=.+/)[0].substr(1));
+        var img_url = decodeURIComponent(this.href.match(/url=[^&]+/)[0].match(/=.+/)[0].substr(1));
         //this.style.position = 'relative';
-        jQuery(this.parentNode).append('<div style="background-color:white;position:absolute;top:0px" id="' + id + '" onclick="event.cancelBubble=true"><a href="' + imgurl + '" target="_blank">Open image</a></div>')
+        jQuery(this.parentNode).append('<div style="background-color:white;position:absolute;top:0px" id="' + id + '" onclick="event.cancelBubble=true"><a href="' + img_url + '" target="_blank">Open image</a></div>')
       });
     });
   }
@@ -39,7 +39,11 @@ if (/^https:\/\/www.google.[^\/]+\/search/.test(location.href)) {
     if (videos.length == 1) {
       var video = videos[0];
       video.parentNode.onclick = function () {
+        if (this.loaded) return;
+        this.loaded = true;
         var src = video.firstElementChild.src;
+        src = src.replace(/\/\d+$/, '');
+        console.log('Found video ' + src + ' at ' + location.href);
         document.body.innerHTML = '<video style="width:100%;height:100%;margin:0px" controls loop src="' + src + '"></video>';
       }
     }
